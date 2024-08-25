a87787208f03c0128cae361fef79f8b85297fb18
nichenqin <nichenqin@hotmail.com>
2024-08-25 10:28:00 +0800

    chore: add track

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script>
+	import { dev } from '$app/environment';
 	import '../app.css';
-    import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
+
 <ModeWatcher />
 
 <slot></slot>
+
+<svelte:head>
+	{#if !dev}
+		<script
+			async
+			defer
+			src="http://89.116.212.166:12345/tracker.js"
+			data-website-id="cm08y36hv0001wxcraa1zy1dm"
+		></script>
+	{/if}
+</svelte:head>
