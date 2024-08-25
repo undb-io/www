9121fdd34875cf57b9f8dabd3c1f8116c65d1ff7
nichenqin <nichenqin@hotmail.com>
2024-08-25 10:45:05 +0800

    chore: seo title

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 export const seo = {
-	title: 'Undb',
+	title: 'Undb | Open Source Airtable Alternative & Backend as a Service',
 	description:
 		'undb is a local-first, offline-first no-code platform that empowers you to build and manage your own databases with ease. Start creating your type-safe APIs and manage your data securely. An Airtable alternative.',
 	image: '',
