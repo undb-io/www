9dc99abd2f02566ab4de8aba19331fe311288839
nichenqin <nichenqin@hotmail.com>
2024-09-13 15:06:03 +0800

    blog: add gallery view blog

--- src/lib/components/blog/article.svelte
+++ src/lib/components/blog/article.svelte
@@ -1,6 +1,10 @@
-<article>
+<article class="prose lg:prose-xl" itemtype="https://schema.org/BlogPosting">
 	<slot />
 </article>
+
+<svelte:head>
+	<!-- head content -->
+</svelte:head>
 
 <style>
 	article {
