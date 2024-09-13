3e25311fd861727d88ccb59e7fc4f69b64800e56
nichenqin <nichenqin@hotmail.com>
2024-09-13 18:21:29 +0800

    chore: add x

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -1,5 +1,7 @@
 <script>
 	import DiscordSvg from '$lib/imgs/discord.svg';
+	import { TwitterIcon } from 'lucide-svelte';
+	import XSvg from '$lib/imgs/x.svg';
 	import Logo from '$lib/imgs/logo.svg';
 
 	const footerNavs = [
@@ -33,6 +35,10 @@
 					name: 'Discord'
 				},
 				{
+					href: 'https://x.com/unified_undb',
+					name: 'X'
+				},
+				{
 					href: 'mailto:hi@undb.io',
 					name: 'Email'
 				}
@@ -58,6 +64,11 @@
 			href: 'https://discord.gg/3rcNdU3y3U',
 			name: 'Discord',
 			icon: DiscordSvg
+		},
+		{
+			href: 'https://x.com/unified_undb',
+			name: 'X',
+			icon: TwitterIcon
 		}
 	];
 </script>
@@ -100,18 +111,35 @@
 		</div>
 
 		<div
-			class="flex flex-col gap-2 rounded-md border-neutral-700/20 px-8 py-4 sm:flex sm:flex-row sm:items-center sm:justify-between"
+			class="flex flex-col gap-2 rounded-md border-neutral-700/20 px-8 py-4 pb-20 sm:flex sm:flex-row sm:items-center sm:justify-between"
 		>
 			<div class="flex items-center space-x-5 sm:mt-0 sm:justify-center">
-				{#each footerSocials as social}
-					<a
-						href={social.href}
-						class="fill-gray-500 text-gray-500 hover:fill-gray-900 hover:text-gray-900 dark:hover:fill-gray-600 dark:hover:text-gray-600"
+				<a
+					href="https://x.com/unified_undb"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="fill-gray-500 text-gray-500 hover:fill-gray-900 hover:text-gray-900 dark:hover:fill-gray-600 dark:hover:text-gray-600"
+				>
+					<svg role="img" class="size-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+						><title>X</title><path
+							d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
+						/></svg
 					>
-						<img src={social.icon} class="size-4" alt={social.name} />
-						<span class="sr-only">{social.name}</span>
-					</a>
-				{/each}
+					<span class="sr-only">X</span>
+				</a>
+
+				<a
+					href="https://discord.gg/3rcNdU3y3U"
+					target="_blank"
+					rel="noopener noreferrer"
+					class="fill-gray-500 text-gray-500 hover:fill-gray-900 hover:text-gray-900 dark:hover:fill-gray-600 dark:hover:text-gray-600"
+				>
+					<svg role="img" viewBox="0 0 24 24" class="size-5" xmlns="http://www.w3.org/2000/svg"
+						><title>Discord</title><path
+							d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"
+						/></svg
+					> <span class="sr-only">Discord</span>
+				</a>
 			</div>
 			<span class="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
 				Copyright ©
