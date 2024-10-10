002ba0e80fd768ffdf332b8ace4972c77189969d
nichenqin <nichenqin@hotmail.com>
2024-10-10 09:58:31 +0800

    chore: seo

--- src/lib/components/landing/HeroSection.svelte
+++ src/lib/components/landing/HeroSection.svelte
@@ -50,10 +50,10 @@
 	<h1
 		class="-translate-y-4 animate-fade-in text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl"
 	>
-		Create and share your own database
+		Build your own Application
 		<br class="hidden md:block" />
 		{' '}
-		with ease.
+		in minutes.
 	</h1>
 	<p
 		class="mb-12 -translate-y-4 animate-fade-in text-balance text-lg tracking-tight text-gray-400 opacity-0 [--animation-delay:400ms] md:text-xl"
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,9 +3,10 @@
 export const seo = {
 	title: 'Undb | Open Source Airtable Alternative & Backend as a Service',
 	description:
-		'undb is a local-first, offline-first no-code platform that empowers you to build and manage your own databases with ease. Start creating your type-safe APIs and manage your data securely. An Airtable alternative.',
+		'undb is a local-first, offline-first no-code platform that empowers you to build and manage your own databases with ease. Start creating your type-safe APIs and manage your data securely. Build your own application in minutes. An Airtable alternative.',
 	image: '',
 	twitter: 'unified_undb',
 	url: 'https://undb.io',
-	keywords: 'nocode platform, airtable alternative, backend as a service, type-safe api, sqlite'
+	keywords:
+		'nocode platform, Airtable alternative, backend as a service, type-safe API, SQLite, build your own application'
 };
