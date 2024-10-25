36efb92d046b207998fe7dd7d7f7abd1fb91d3b7
nichenqin <nichenqin@hotmail.com>
2024-10-25 20:10:50 +0800

    chore: use bun

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,38 +1,43 @@
 <script lang="ts">
 	import { seo } from '$lib';
 	import HeroSection from '$lib/components/landing/HeroSection.svelte';
-	import RoadmapSection from '$lib/components/landing/RoadmapSection.svelte';
 	import SphereMask from '$lib/components/magic/SphereMask/SphereMask.svelte';
-	import ViewsSection from '$lib/components/landing/ViewsSection.svelte';
-	import TemplateSection from '$lib/components/landing/TemplateSection.svelte';
-	import PricingSection from '$lib/components/landing/PricingSection.svelte';
-	import StackSection from '$lib/components/landing/StackSection.svelte';
-	import SubmitBugSection from '$lib/components/landing/SubmitBugSection.svelte';
 </script>
 
 <svelte:head>
 	<title>{seo.title}</title>
 	<meta name="description" content={seo.description} />
 	<meta name="keywords" content={seo.keywords} />
-	<!-- 其他元标签保持不变 -->
 </svelte:head>
 
 <HeroSection />
 <SphereMask />
 
-<ViewsSection />
-<SphereMask />
+{#await import('$lib/components/landing/ViewsSection.svelte') then { default: ViewsSection }}
+	<ViewsSection />
+	<SphereMask />
+{/await}
 
-<TemplateSection />
-<SphereMask />
+{#await import('$lib/components/landing/TemplateSection.svelte') then { default: TemplateSection }}
+	<TemplateSection />
+	<SphereMask />
+{/await}
 
-<PricingSection />
-<SphereMask />
+{#await import('$lib/components/landing/PricingSection.svelte') then { default: PricingSection }}
+	<PricingSection />
+	<SphereMask />
+{/await}
 
-<RoadmapSection />
-<SphereMask />
+{#await import('$lib/components/landing/RoadmapSection.svelte') then { default: RoadmapSection }}
+	<RoadmapSection />
+	<SphereMask />
+{/await}
 
-<StackSection />
-<SphereMask />
+{#await import('$lib/components/landing/StackSection.svelte') then { default: StackSection }}
+	<StackSection />
+	<SphereMask />
+{/await}
 
-<SubmitBugSection />
+{#await import('$lib/components/landing/SubmitBugSection.svelte') then { default: SubmitBugSection }}
+	<SubmitBugSection />
+{/await}
