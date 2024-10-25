791fd2e0731a712a89a9a851ef54d3d322c7a205
nichenqin <nichenqin@hotmail.com>
2024-10-25 11:11:00 +0800

    chore: update package.json

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,22 +4,6 @@
 	import SphereMask from '$lib/components/magic/SphereMask/SphereMask.svelte';
 	import { onMount } from 'svelte';
 	import type { Component } from 'svelte';
-
-	let ViewsSection: Component,
-		TemplateSection: Component,
-		PricingSection: Component,
-		RoadmapSection: Component,
-		StackSection: Component,
-		SubmitBugSection: Component;
-
-	onMount(async () => {
-		ViewsSection = (await import('$lib/components/landing/ViewsSection.svelte')).default;
-		TemplateSection = (await import('$lib/components/landing/TemplateSection.svelte')).default;
-		PricingSection = (await import('$lib/components/landing/PricingSection.svelte')).default;
-		RoadmapSection = (await import('$lib/components/landing/RoadmapSection.svelte')).default;
-		StackSection = (await import('$lib/components/landing/StackSection.svelte')).default;
-		SubmitBugSection = (await import('$lib/components/landing/SubmitBugSection.svelte')).default;
-	});
 </script>
 
 <svelte:head>
@@ -32,12 +16,12 @@
 <HeroSection />
 <SphereMask />
 
-{#if ViewsSection}
-	<svelte:component this={ViewsSection} />
+{#await import('$lib/components/landing/ViewsSection.svelte') then { default: ViewsSection }}
+	<ViewsSection />
 	<SphereMask />
-{/if}
+{/await}
 
-{#if TemplateSection}
+<!-- {#if TemplateSection}
 	<svelte:component this={TemplateSection} />
 	<SphereMask />
 {/if}
@@ -59,4 +43,4 @@
 
 {#if SubmitBugSection}
 	<svelte:component this={SubmitBugSection} />
-{/if}
+{/if} -->
